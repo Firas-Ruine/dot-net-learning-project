e29d89dce663dec1a636bbab98f7e9ac2b591c11
Yassine <medyassinebenromdhane00@gmail.com>
2024-02-23 20:07:15 +0100

    add register user logic with validation of each input

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using e_learning.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ELearningContext>();
 
 var app = builder.Build();
 
